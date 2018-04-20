e4455bd793bd2b1d74c2d6fc849d4ac438a4a1ae
Rakesh  Chatrath <rchatrath7@gmail.com>
2018-04-19 20:41:37 -0500

    Update parser

--- parse_api.py
+++ parse_api.py
@@ -8,33 +8,34 @@ import collections
 class ParseAPI: 
     def __init__(self, target, consumes=5):
     	self.URL = "https://www.ncbi.nlm.nih.gov/nucest/"
-	self.target = target
-	self.consumes = consumes
-	self.queue = Queue.PriorityQueue(len(self.target))
+    	self.target = target
+    	self.consumes = consumes
+    	self.queue = Queue.PriorityQueue(len(self.target))
 
     @staticmethod
     def _retrieve_from_url(url):
         print "Getting url: " + url 
         http = httplib2.Http()
-	status, response = http.request(url)
-	soup = BeautifulSoup(response, 'html.parser')
+    	status, response = http.request(url)
+    	soup = BeautifulSoup(response, 'html.parser')
         gene_layer = soup.findAll("div", {"class": "portlet_title"})
-	return gene_layer 
+	
+        return gene_layer 
    
     def get_gene(self, gb_accnum):
         layer = self._retrieve_from_url(self.URL + gb_accnum)
         gene_code = "" 
-	layer = [row.text.replace('\n','') for row in layer]
-	for context in layer:
-	    if 'ESTs' in context:
-	        gene_code = context.replace("ESTs for the", '').replace(' gene', '') 	
-                return gene_code
+    	layer = [row.text.replace('\n','') for row in layer]
+    	for context in layer:
+    	    if 'ESTs' in context:
+    	        gene_code = context.replace("ESTs for the", '').replace(' gene', '') 	
+                    return gene_code
         return ""
     
     def wrapper(self, *args):
 
         @Worker(target=self.target, consumes=self.consumes)
-	def work(*args):
+        def work(*args):
             target = args[0]
             gene = parser.get_gene(target[1])
             self.queue.put((target[0], gene))
